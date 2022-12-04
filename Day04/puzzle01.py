4337fff22217fcda1e11416d9aedfcf3cbf07ea4
Youssouf da Silva <youssoufdasilva@gmail.com>
2022-12-04 19:38:46 +0000

    Completed First Puzzle of Day 04

---
+++ Day04/puzzle01.py
@@ -0,0 +1,83 @@
+# filename = "testinput.txt"
+filename = "input.txt"
+
+def has_full_overlap(elf_assignments):
+    left_assignment = elf_assignments[0].strip().split("-")
+    right_assignment = elf_assignments[1].strip().split("-")
+
+    start_left = int(left_assignment[0])
+    end_left = int(left_assignment[1])
+
+    start_right = int(right_assignment[0])
+    end_right = int(right_assignment[1])
+
+    left_is_fully_contained = False
+    right_is_fully_contained = False
+
+    # if (start_right == end_right):
+    #     if ()
+    # el
+    if (start_left >= start_right and end_left<= end_right):
+        left_is_fully_contained = True
+    
+    if (start_right>=start_left and end_right<=end_left):
+        right_is_fully_contained = True
+
+    return left_is_fully_contained or right_is_fully_contained
+
+def has_overlap(elf_assignments):
+    detection_dict = {}
+
+    left_assignment = elf_assignments[0].strip().split("-")
+    right_assignment = elf_assignments[1].strip().split("-")
+
+    # print("Left Assignment :: ", left_assignment)
+    # print("Right Assignment :: ", right_assignment)
+
+    start_left = int(left_assignment[0])
+    end_left = int(left_assignment[1]) + 1
+    # end_left = int(left_assignment[1]) + 1 if start_left == int(left_assignment[1]) else int(left_assignment[1])
+
+    start_right = int(right_assignment[0])
+    end_right = int(right_assignment[1]) + 1
+    # end_right = int(right_assignment[1]) + 1 if start_right == int(right_assignment[1]) else int(right_assignment[1])
+
+    for assignment_id in range(start_left, end_left):
+        my_id = str(assignment_id)
+        if my_id in detection_dict:
+            detection_dict[my_id] += 1
+        else:
+            detection_dict[my_id] = 1
+
+    for assignment_id in range(start_right, end_right):
+        my_id = str(assignment_id)
+        if my_id in detection_dict:
+            detection_dict[my_id] += 1
+        else:
+            detection_dict[my_id] = 1
+        
+    print(detection_dict)
+
+    for dict_item in detection_dict.items():
+        if dict_item[1] > 1:
+            return True
+    
+    return False
+
+all_assignments = open(filename, 'r', encoding='utf8').read().strip().split("\n")
+
+# check_assignment_overlap = has_overlap(all_assignments[0].strip().split(","))
+# print("check_overlap :: ", check_assignment_overlap)
+
+# check_assignment_overlap = has_overlap(all_assignments[17].strip().split(","))
+# print("check_overlap :: ", check_assignment_overlap)
+
+overlap_counter = 0
+
+for elf_assignment in all_assignments:
+    print("\nElf Assignment Pair: ", elf_assignment)
+    if has_full_overlap(elf_assignment.strip().split(",")):
+        print("Has Overlap!")
+        overlap_counter += 1
+
+print("overlap_counter = ", overlap_counter)
